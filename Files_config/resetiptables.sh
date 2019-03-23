11cb2c54197d0df0cd3003967f6a6f35d63450ee
Nicolas VIENOT <nvienot@student.42.fr>
2019-03-23 14:35:50 +0100

    ok

---
+++ Files_config/resetiptables.sh
@@ -0,0 +1,10 @@
+#Flush iptables
+sudo iptables -F
+sudo iptables -X
+sudo iptables -t nat -F
+sudo iptables -t nat -X
+sudo iptables -t mangle -F
+sudo iptables -t mangle -X
+sudo iptables -P INPUT ACCEPT
+sudo iptables -P FORWARD ACCEPT
+sudo iptables -P OUTPUT ACCEPT:X
