ea30cecd4707bf86748af5059471788f2e0f349b
Nicolas VIENOT <nvienot@student.42.fr>
2019-03-24 14:08:55 +0100

    100/100

--- Files_config/crontab_script.sh
+++ Files_config/crontab_script.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+
 cat /etc/crontab > /home/nico/crontab_save/new
+
 DIFF=$(diff /home/nico/crontab_save/new /home/nico/crontab_save/crontab)
+
 if [ "$DIFF" != "" ]; then
-	sudo echo "Crontab has changed, sending mail"
+	echo "Crontab has changed, sending mail!"
 	sudo sendmail nvienot@student.42.fr < /home/nico/mail/mail_crontab.txt
-	sudo rm -rf /home/nico/crontab_save/crontab
-	sudo mv /home/nico/crontab_save/new /home/nico/crontab_save/crontab
-	sudo rm -rf /home/nico/crontab_save/new
+	rm /home/nico/crontab_save/crontab
+	mv /home/nico/crontab_save/new /home/nico/crontab_save/crontab
+else
+	echo "No changes on crontab!"
 fi
+
+exit 0
